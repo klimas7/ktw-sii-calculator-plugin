f4fd80488e3f4857acd48f0b0c90386c670db600
klimas7 <klimas7@gmail.com>
2018-03-11 15:43:53 +0100

    support numerical system in Descriptor

--- src/main/java/io/jenkins/plugins/calculatorPlugin/CalculatorBuilder.java
+++ src/main/java/io/jenkins/plugins/calculatorPlugin/CalculatorBuilder.java
@@ -7,12 +7,15 @@ import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.AbstractProject;
+import hudson.model.Descriptor;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
 import jenkins.tasks.SimpleBuildStep;
+import net.sf.json.JSONObject;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
 
 public class CalculatorBuilder extends Builder implements SimpleBuildStep {
 
@@ -28,6 +31,11 @@ public class CalculatorBuilder extends Builder implements SimpleBuildStep {
 
     @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+        private NumericalSystem numericalSystem = NumericalSystem.HEX;
+
+        public DescriptorImpl() {
+            load();
+        }
 
         @Override
         public boolean isApplicable(Class<? extends AbstractProject> jobType) {
@@ -38,6 +46,17 @@ public class CalculatorBuilder extends Builder implements SimpleBuildStep {
         @Override
         public String getDisplayName() {
             return "Calculate";
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+            numericalSystem = NumericalSystem.valueOf((String) json.get("numericalSystem"));
+            save();
+            return super.configure(req, json);
+        }
+
+        public NumericalSystem getNumericalSystem() {
+            return numericalSystem;
         }
     }
 
